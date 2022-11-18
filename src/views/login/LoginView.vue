0f579e297073dd934efae9af66b5af0f5be221c7
zy <varzy@live.com>
2022-11-18 22:10:39 +0800

    Update

--- src/views/login/LoginView.vue
+++ src/views/login/LoginView.vue
@@ -1,5 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive, ref } from 'vue';
+import { LoginReq } from '@/types/altria';
+
+const form = reactive<LoginReq>({
+  username: '',
+  password: '',
+});
+</script>
 
 <template>
-  <div class="login">login</div>
+  <div class="login">
+    <div class="form-wrapper"></div>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .form-wrapper {
+    width: 600px;
+  }
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 8080,
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/resources.scss";`,
+        javascriptEnabled: true,
+      },
+    },
   },
 });
